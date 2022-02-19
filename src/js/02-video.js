1602d714ba50de1710831e4e384845b146665f23
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-02-19 22:46:48 +0200

    second-task-completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle  from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const timeToResume = JSON.parse(localStorage.getItem("videoplayer-current-time") || 0);
+player.setCurrentTime(timeToResume);
+
+player.on('timeupdate', throttle(getTime,1000));
+  
+  function getTime(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds)
+    console.log(event.seconds);
+};
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });

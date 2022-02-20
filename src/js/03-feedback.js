75ff3ecfbddab5392128d0af4d375c27a0bf2e88
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-02-20 12:45:37 +0200

    third task completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector(".feedback-form");
+const emailRef = document.querySelector('[name="email"]');
+const messageRef = document.querySelector('[name="message"]');
+
+let savedData = {};
+
+updateForm();
+
+formRef.addEventListener('input', throttle(onType,500));
+
+
+function onType(event) {
+  savedData[event.target.name] = event.target.value;
+  localStorage.setItem("feedback-form-state",
+    JSON.stringify(savedData));
+  console.log(localStorage.getItem("feedback-form-state"))
+};
+
+function updateForm() {
+  if (!localStorage.getItem("feedback-form-state")) {
+    return
+  };
+  savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+  emailRef.value = savedData[emailRef.name];
+  messageRef.value = savedData[messageRef.name];
+};
+formRef.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+  localStorage.removeItem("feedback-form-state");
+  console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+  formRef.reset();
+};
